336933552923c976134399bea13ca22b05a0ea84
oopsmails <oopsmails@gmail.com>
2017-12-15 08:50:44 -0500

    20-Project Authentication and Authorization: 01, auth with google, before revising auth to service

--- oshop/src/app/bs-navbar/bs-navbar.component.ts
+++ oshop/src/app/bs-navbar/bs-navbar.component.ts
@@ -1,15 +1,24 @@
+import { Observable } from 'rxjs/Observable';
 import { Component, OnInit } from '@angular/core';
+import { AngularFireAuth } from 'angularfire2/auth';
+import * as firebase from 'firebase';
 
 @Component({
+  // tslint:disable-next-line:component-selector
   selector: 'bs-navbar',
   templateUrl: './bs-navbar.component.html',
   styleUrls: ['./bs-navbar.component.css']
 })
-export class BsNavbarComponent implements OnInit {
+export class BsNavbarComponent {
+  user$: Observable<firebase.User>;
 
-  constructor() { }
+  constructor(private afAuth: AngularFireAuth) {
+    // afAuth.authState.subscribe(user => this.user = user);
+    this.user$ = afAuth.authState;
+  }
 
-  ngOnInit() {
+  logout() {
+    this.afAuth.auth.signOut();
   }
 
 }
